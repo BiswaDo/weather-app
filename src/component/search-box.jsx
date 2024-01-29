8713ccb6be11c287dcfcc0b81225c8818c250b15
BiswaDo <biswadipsaha503@gmail.com>
2024-01-29 20:05:30 +0530

    few updates

--- src/component/search-box.jsx
+++ src/component/search-box.jsx
@@ -1,82 +1,72 @@
 
 
 function searchBox(){
-    const API_key = "e807385dc4cef16d9f5b3aaa9fac79ca";
+  const API_key = "e807385dc4cef16d9f5b3aaa9fac79ca";
 
+  async function search() {
+    const placeInput = document.querySelector(".place");
+    const tempDisplay = document.querySelector(".temp");
+    const timeDisplay = document.querySelector(".time");
+    const descriptionDisplay = document.querySelector(".des");
+    const mainIcon = document.querySelector(".main-icon");
+    const humidityDisplay = document.querySelector(".humidity");
+    const pressureDisplay = document.querySelector(".pressure");
+    const speedDisplay = document.querySelector(".speed");
 
-    
-    
-    async function search(){
-    const element = document.getElementsByClassName("place")[0];
-    console.log(element);
-    if (element.value===""){
+    if (placeInput.value === "") {
       return 0;
     }
-        
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element.value}&appid=${API_key}&units=metric`;
-    
-    let response = await fetch(url);
-    let data = await response.json()
-    const temp = document.getElementsByClassName("temp")[0];
-    temp.innerHTML = Math.floor(data.main.temp);
-    
-    let date = new Date();
-    let current_time = date.toLocaleString([], {
-        hour: '2-digit',
-        minute: '2-digit'
-    });
-    
-    const time = document.getElementsByClassName("time");
-    console.log(time);
-    time[0].innerHTML = current_time;
-    
-    let desc = data.weather[0].description;
-    const description = document.getElementsByClassName("des")[0];
-    description.innerHTML=desc;
-    let setIcon = document.getElementsByClassName("main-icon")[0];
 
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${placeInput.value}&appid=${API_key}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    tempDisplay.innerHTML = Math.floor(data.main.temp);
+
+    const date = new Date();
+    const current_time = date.toLocaleString([], {
+      hour: '2-digit',
+      minute: '2-digit'
+    });
+    timeDisplay.innerHTML = current_time;
+
+    const desc = data.weather[0].description;
+    descriptionDisplay.innerHTML = desc;
 
     const iconMapping = {
-    "01d": "01d@4x.png",
-    "02d": "02d@4x.png",
-    "03d": "03d@4x.png",
-    "04d": "04d@4x.png",
-    "09d": "09d@4x.png",
-    "10d": "10d@4x.png",
-    "11d": "11d@4x.png",
-    "13d": "13d@4x.png",
-    "50d": "50d@4x.png",
-    "01n": "01n@4x.png",
-    "02n": "02n@4x.png",
-    "03n": "03n@4x.png",
-    "04n": "04n@4x.png",
-    "09n": "09n@4x.png",
-    "10n": "10n@4x.png",
-    "11n": "11n@4x.png",
-    "13n": "13n@4x.png",
-    "50n": "50n@4x.png",
-};
+      "01d": "01d@4x.png",
+      "02d": "02d@4x.png",
+      "03d": "03d@4x.png",
+      "04d": "04d@4x.png",
+      "09d": "09d@4x.png",
+      "10d": "10d@4x.png",
+      "11d": "11d@4x.png",
+      "13d": "13d@4x.png",
+      "50d": "50d@4x.png",
+      "01n": "01n@4x.png",
+      "02n": "02n@4x.png",
+      "03n": "03n@4x.png",
+      "04n": "04n@4x.png",
+      "09n": "09n@4x.png",
+      "10n": "10n@4x.png",
+      "11n": "11n@4x.png",
+      "13n": "13n@4x.png",
+      "50n": "50n@4x.png",
+    };
 
-const iconCode = data.weather[0].icon;
-const iconUrl = `https://openweathermap.org/img/wn/${iconMapping[iconCode]}`;
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/wn/${iconMapping[iconCode]}`;
+    mainIcon.src = iconUrl;
 
-setIcon.src = iconUrl;
+    const humidity = data.main.humidity;
+    const pressure = (data.main.pressure * 0.0145037738).toFixed(2);
+    const speed = Math.floor(data.wind.speed);
 
-let humidity = data.main.humidity;
-let pressure = data.main.pressure;
-let speed = data.wind.speed;
-let humidity_icon =  document.getElementsByClassName("humidity")[0];
-let pressure_icon =  document.getElementsByClassName("pressure")[0];
-let speed_icon =  document.getElementsByClassName("speed")[0];
-
-humidity_icon.innerHTML = humidity+" %";
-pressure_icon.innerHTML = (pressure * 0.0145037738).toFixed(2)+" psi";
-speed_icon.innerHTML = Math.floor(speed)+" m/s";
-
-    
-    
-    
+    humidityDisplay.innerHTML = `${humidity} %`;
+    pressureDisplay.innerHTML = `${pressure} psi`;
+    speedDisplay.innerHTML = `${speed} m/s`;
   }
+
 
     return(
         <div className="search-box">
